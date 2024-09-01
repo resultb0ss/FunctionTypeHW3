205256eb8dbbe8b40f020c43ab47bd7423533f79
Admin <reteamboss@gmail.com>
2024-09-01 21:06:24 +0300

    Задание 3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,16 @@
 fun main() {
-    println("Hello World!")
+    var array = arrayOf(1,2,3,4,5,6,7)
+    fuctionOne(array)
+
+
+}
+
+fun fuctionOne(array: Array<Int>){
+
+    for (i in array.indices step 2){
+        array[i] = array[i] + 1
+
+    }
+    println(array.contentToString())
+
 }
